f0ee07b847edc59049dfbabf40c27a103ea80cd8
Vytautas Helstein <vytautas.helstein@gmail.com>
2014-10-30 21:01:33 +0200

    Challenges widget visibility fix, code validation

--- app/js/directives/challengesAdvertiser.js
+++ app/js/directives/challengesAdvertiser.js
@@ -1,9 +1,18 @@
-﻿
+﻿/*
+ * Copyright (C) 2014 TopCoder Inc., All Rights Reserved.
+ */
+/**
+ * Directive for challenges advertising widget.
+ *
+ * @author Helstein
+ * @version 1.0
+ */
+
 'use strict';
 /*jshint -W097*/
 /*jshint strict:false*/
+/*global module*/
 
-// Directive for challenges advertising widget
 var challengesAdvertiser = [function () {
     return {
         restrict: 'A',
